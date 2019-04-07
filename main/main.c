560307e3ee0e630af828910a925c5626bb92b9a3
刘瑞瑞 <liuruiruijob@126.com>
2019-04-07 05:45:57 -0700

    Delete uart config unused

--- main/main.c
+++ main/main.c
@@ -82,19 +82,6 @@ void wifi_init_softap()
 {
     wifi_event_group = xEventGroupCreate();
 
-//    /* Configure parameters of an UART driver,
-//     * communication pins and install the driver */
-//    uart_config_t uart_config = {
-//        .baud_rate = 115200,
-//        .data_bits = UART_DATA_8_BITS,
-//        .parity    = UART_PARITY_DISABLE,
-//        .stop_bits = UART_STOP_BITS_1,
-//        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-//    };
-//    uart_param_config(UART_NUM_1, &uart_config);
-//    uart_set_pin(UART_NUM_1, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS);
-//    uart_driver_install(UART_NUM_1, BUF_SIZE * 2, 0, 0, NULL, 0);
-
     tcpip_adapter_init();
     ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
 
